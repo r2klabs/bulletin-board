13eca262584f1aa479ea239445d53c4d5bdaa8df
r2klabs <rrkelley1225@mail.com>
2022-10-30 10:44:50 -0400

    Updated default entries

--- backend/events.js
+++ backend/events.js
@@ -1,19 +1,20 @@
 module.exports = [
     {
       id: 1,
-      title: 'Docker Workshop',
-      detail: 'Linuxing in Lonodon ',
-      date: '2017-11-21'
+      title: 'Career Pathways Dinner',
+      detail: 'Kaelin\'s 80/20',
+      date: '2022-10-31'
     },
     {
       id: 2,
-      title: 'WinOps #17',
-      detail: 'WinOps London',
-      date: '2017-11-21'
+      title: 'Bullitt Math Collection - UofL ',
+      detail: 'Tour of the BMC at UofL with Dr. White',
+      date: '2022-11-3'
     },
     {
       id: 3,
-      title: 'Docker London',
-      date: '2017-11-13'
+      title: 'KY Convergence',
+      detail: 'Online Course Development Workshop',
+      date: '2022-11-10'
     }
   ];
